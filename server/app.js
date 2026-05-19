9a841dd9bd2f31ff9f8321fa2416a6bb66f7dc4a
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-05-20 01:01:41 +0530

    initial server create

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app=express();
+
+app.use('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port 3000");
+});
