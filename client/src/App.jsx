2a042ae10d90d2d51058cabf49283636b618f163
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-07-13 15:56:42 +0530

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import Navbar from "./components/Navbar";
-function App() {
+import Body from "./components/Body";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
+function App() {
   return (
     <>
-    <Navbar></Navbar>
-       <h1 className="text-4xl font-bold text-blue-600">
-      Tailwind CSS is working!
-    </h1>
-
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
