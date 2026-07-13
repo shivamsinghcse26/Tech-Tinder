c35bd68858cc8f2948384367d196c4dbf17d6fc3
Shivam Kumar Singh <000shiv2304@gmail.com>
2026-07-13 14:47:59 +0530

    add navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+function App() {
+
+  return (
+    <>
+    <Navbar></Navbar>
+       <h1 className="text-4xl font-bold text-blue-600">
+      Tailwind CSS is working!
+    </h1>
+
+    </>
+  )
+}
+
+export default App
